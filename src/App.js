1d1f052f0f0c2213dabeb97aa8af1c3d6175c88f
Hemalpatel007 <97945840+Hemalpatel007@users.noreply.github.com>
2022-01-24 18:42:09 +0530

    com

--- src/App.js
+++ src/App.js
@@ -4,22 +4,51 @@ import './App.css';
 const App = () => {
   const [todo, setTodo] = useState("");
   const [todos, setTodos] = useState([]);
+  const [editId, setEditId] = useState(0);
 
     const handleSubmit = (e) => {
       e.preventDefault();
 
+      if (editId) {
+        const editTodo=todos.find((i) => i.id === editId);
+        const updatedTodos=todos.map((t) => 
+          t.id === editTodo.id 
+            ? (t = { id: t.id, todo })
+            : { id: t.id, todo: t.todo }
+      );
+      setTodos(updatedTodos);
+      setEditId(e)
+      setTodo("")
+      return;
+      }
+
       if (todo !== '') {
         //setTodos([{ id: `${todo}-${Date.now()}`, todo }, ...todos])
         setTodos([{ id: `${todo}-${Date.now()}` ,todo }, ...todos])
+        setTodo("");
       }
     };
+
+    const handleDelete = (id) => {
+      const delTodo=todos.filter((to) => to.id !== id);
+      setTodos([...delTodo]);
+    };
+
+    const handleEdit = (id) => {
+
+      const editTodo = todos.find((i) => i.id === id);
+      setTodo(editTodo.todo);
+      setEditId(id);
+
+    };
+
 
   return (<div className="App">
     <div className="container">
       <h1>Ncode Todo List App</h1>
       <form className="todoForm" onSubmit={handleSubmit}>
-         <input type="text" onChange={(e)=>setTodo(e.target.value)} />
-         <button type="submit">Go</button>
+         <input type="text" value={todo} onChange={(e)=>setTodo(e.target.value)} />
+         <button className='sum-btn' type="submit"> {editId ? "Edit" : "Go"}</button>
       </form>
 
       <ul className="allTodos">
@@ -28,8 +57,8 @@ const App = () => {
               <span className="todoText">
                 {t.todo}
               </span>
-              <button>Edit</button>
-              <button>Delete</button>
+              <button className='edit-btn' onClick={() => handleEdit(t.id)}>Edit</button>
+              <button className='del-btn' onClick={() => handleDelete(t.id)}>Delete</button>
             </li> 
           ))}
       </ul>
